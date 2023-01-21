a0203d4ee7017b83403ea38bbbef15b2dc06be60
Agromos sp <101366474+Agromos0@users.noreply.github.com>
2023-01-21 01:13:01 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['595983186566', 'AGROMOS SEXY', true], 
-  ['18178659441'], 
+  ['523931806576', 'ANGEL YT', true], 
+  ['523931678470'], 
   ['18178659441']
 ] //Numeros de owner 
 
